35969c6438bca05bb79c1ae8bc9aff4839457902
Cyril Li <cyrils.li@icloud.com>
2025-01-04 21:52:11 -0600

    export

--- accelerometer.py
+++ accelerometer.py
@@ -1,4 +1,5 @@
 import serial, threading, time
+from getpass import getpass
 from math import atan2, degrees, sqrt
 
 ser = serial.Serial ("/dev/ttyACM1", 115200)
@@ -35,11 +36,20 @@ avg_x = sum (i [0] for i in old_data) / len (old_data)
 avg_y = sum (i [2] for i in old_data) / len (old_data)
 print ("Average values:", avg_x, avg_y)
 
+points = []
 input ("Start recording acceleration? ")
-while True:
-    data = read_data ()
-    if data:
-        heading = round (degrees (atan2 (data [0] - avg_x, data [2] - avg_y)))
-        magnitude = round (sqrt ((data [0] - avg_x) ** 2 + (data [2] - avg_y) ** 2))
-        if magnitude > threshold:
-            print (heading, magnitude)
+try:
+    while True:
+        data = read_data ()
+        ts = time.time_ns ()
+        if data:
+            heading = round (degrees (atan2 (data [0] - avg_x, data [2] - avg_y)))
+            magnitude = round (sqrt ((data [0] - avg_x) ** 2 + (data [2] - avg_y) ** 2))
+            if magnitude > threshold:
+                points.extend ((heading, magnitude, ts // 1000000))
+                print ("+", end = "", flush = True)
+except KeyboardInterrupt:
+    pass
+
+print (f"\nRecording done, {len (points)} points recorded.")
+pos_points = getpass ("Please paste the list of position points: ").strip ().split (",")
